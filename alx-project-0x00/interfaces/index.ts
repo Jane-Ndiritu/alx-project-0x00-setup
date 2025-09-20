f6b0aecd159e5a3663d8d000c1fe33af9cb1dea1
jane <jayndiritu36@gmail.com>
2025-09-20 22:22:11 +0300

    creating buttons

--- alx-project-0x00/interfaces/index.ts
+++ alx-project-0x00/interfaces/index.ts
@@ -1,9 +1,6 @@
-// export interface PillProps {
-//   title: string
-// }
 export interface ButtonProps {
   title: string;
-  styles?: string;
+  styles: string;
   size?: 'small' | 'medium' | 'large';
   shape?: 'rounded-sm' | 'rounded-md' | 'rounded-full';
   variant?: 'primary' | 'secondary' | 'success' | 'warning' | 'danger' | 'outline';
