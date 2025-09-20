fb074fc28cdfbbe8d7ac6212663db7e9c403b0c4
jane <jayndiritu36@gmail.com>
2025-09-20 22:31:21 +0300

    creating buttons

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -2,7 +2,8 @@ import Card from "@/components/Card"
 const Landing: React.FC =  () => {
   return (
     <div>
-      <h1 className=" text-xl font-extralight">Landing Page</h1>
+      {/* Added rounded-lg to the heading */}
+      <h1 className="text-xl font-extralight rounded-lg">Landing Page</h1>
       <Card />
     </div>
   )
