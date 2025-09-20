b6a0d2f9fb3a0ba09f70916f8613863469bd1fec
jane <jayndiritu36@gmail.com>
2025-09-20 22:34:41 +0300

    creating buttons

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,12 +1,22 @@
-import Card from "@/components/Card"
-const Landing: React.FC =  () => {
+import Card from "@/components/Card";
+import Button from "@/components/Button"; 
+const Landing: React.FC = () => {
   return (
     <div>
-      {/* Added rounded-lg to the heading */}
-      <h1 className="text-xl font-extralight rounded-lg">Landing Page</h1>
+      <h1 className="text-xl font-extralight">Landing Page</h1>
       <Card />
+      <Button 
+        title="Click Me" 
+        styles="bg-blue-500 text-white mt-4 rounded-sm"
+        onClick={() => console.log('Button clicked!')}
+      />
+      <Button 
+        title="Subscribe" 
+        styles="bg-green-500 text-white mt-4 rounded-full" 
+        onClick={() => console.log('Subscribe clicked!')}
+      />
     </div>
-  )
-}
-  
-export default Landing
+  );
+};
+
+export default Landing;
